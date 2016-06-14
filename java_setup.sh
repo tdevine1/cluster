6b658cba4d4536d8a35cc859963e0f7784fea680
tdevine1 <tdevine1@fairmontstate.edu>
2016-06-14 15:24:13 -0400

    Add files via upload

---
+++ java_setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Script to setup java on FSU cluster
+
+sudo apt-gt-update
+sudo apt-get install openjdk-7-jdk
+apt-cache search jdk
+export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64/bin
+export PATH=$PATH:/usr/lib/jvm/java-7-openjdk-amd64/bin
+javac -version
