dd308521f0b3c77b089af6de173d97920a3567cd
tdevine1 <tdevine1@fairmontstate.edu>
2016-06-23 00:33:32 -0400

    Update java_setup.sh

--- java_setup.sh
+++ java_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Script to setup java on FSU cluster
 
-sudo apt-gt-update
+sudo apt-get-update
 sudo apt-get install openjdk-7-jdk
 apt-cache search jdk
 export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64/bin
