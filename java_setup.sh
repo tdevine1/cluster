099fe61b27be43fa1ea263f9b4fb4b3308b778fa
Unknown <tdevine1@fairmontstate.edu>
2019-04-10 16:47:09 -0400

    host file update for cyrus

--- java_setup.sh
+++ java_setup.sh
@@ -1,8 +1,5 @@
-#!/bin/bash
-# Script to setup java on FSU cluster
-
-sudo apt-get-update
-sudo apt-get install openjdk-7-jdk
+sudo apt update
+sudo apt install openjdk-7-jdk
 apt-cache search jdk
 export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64/bin
 export PATH=$PATH:/usr/lib/jvm/java-7-openjdk-amd64/bin
