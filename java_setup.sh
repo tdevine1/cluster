930d4867b71da314556ed2f9d4af8b8f59d61438
Unknown <tdevine1@fairmontstate.edu>
2019-04-10 16:52:56 -0400

    file cleanup

--- java_setup.sh
+++
@@ -1,6 +0,0 @@
-sudo apt update
-sudo apt install openjdk-7-jdk
-apt-cache search jdk
-export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64/bin
-export PATH=$PATH:/usr/lib/jvm/java-7-openjdk-amd64/bin
-javac -version
