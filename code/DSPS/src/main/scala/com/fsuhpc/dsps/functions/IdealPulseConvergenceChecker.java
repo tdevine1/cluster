69e9b51443b224e8d2e3b67f4b90994adc6433ad
Tom Devine <tdevine1@fairmontstate.edu>
2019-10-14 17:37:18 -0400

    Massive cleanup/reorganzation
    Current code pushed for eclipse scala projects
    Old data and notes put into folders

---
+++ code/DSPS/src/main/scala/com/fsuhpc/dsps/functions/IdealPulseConvergenceChecker.java
@@ -0,0 +1,21 @@
+package com.fsuhpc.dsps.functions;
+
+import org.apache.commons.math3.fitting.leastsquares.LeastSquaresProblem;
+import org.apache.commons.math3.fitting.leastsquares.LeastSquaresProblem.Evaluation;
+import org.apache.commons.math3.optim.ConvergenceChecker;
+import org.apache.commons.math3.util.FastMath;
+
+public class IdealPulseConvergenceChecker<T> implements
+		ConvergenceChecker<LeastSquaresProblem.Evaluation>
+{
+	public boolean converged(int iteration, Evaluation prev, Evaluation cur)
+	{
+		if (FastMath.abs(prev.getRMS() - cur.getRMS()) < 0.0001)
+		{
+			return true;
+		} else
+		{
+			return false;
+		}
+	}
+}
